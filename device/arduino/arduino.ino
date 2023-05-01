e8bcb33208db09d89d4246bb4a16c8704cd7c0f5
Stefano <ste.amedeo@gmail.com>
2023-05-01 18:44:51 +0200

    simplified arduino script

--- device/arduino/arduino.ino
+++ device/arduino/arduino.ino
@@ -7,18 +7,10 @@
 #define TEMPERATURE_PIN A1
 #define LIGHT_PIN A0
 
-int temperatureMinSensorValue, temperatureMaxSensorValue;
-int lightMinSensorValue, lightMaxSensorValue;
-
 void setup()
 {
   analogReference(EXTERNAL);
-
   Serial.begin(9600);
-
-  performCalibration(TEMPERATURE_PIN, &temperatureMinSensorValue, &temperatureMaxSensorValue);
-  performCalibration(LIGHT_PIN, &lightMinSensorValue, &lightMaxSensorValue);
-  sendCalibrationMessage();
 }
 
 void loop()
@@ -38,67 +30,13 @@ float convertSensorValueToTemperature(int sensorValue)
   return temperature;
 }
 
-void performCalibration(uint8_t sensorPin, int *minSensorValue, int *maxSensorValue)
-{
-  unsigned long endCalibrationMillis = millis() + CALIBRATION_DURATION;
-
-  *minSensorValue = 1024;
-  *maxSensorValue = 0;
-
-  while (millis() < endCalibrationMillis)
-  {
-    int sensorValue = analogRead(sensorPin);
-
-    if (sensorValue < *minSensorValue)
-    {
-      *minSensorValue = sensorValue;
-    }
-
-    if (sensorValue > *maxSensorValue)
-    {
-      *maxSensorValue = sensorValue;
-    }
-  }
-
-  *minSensorValue -= 20;
-  *maxSensorValue += 20;
-}
-
-void sendCalibrationMessage()
-{
-  const int capacity = JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(2);
-  StaticJsonDocument<capacity> doc;
-  doc["messageType"] = "CALIBRATION";
-
-  JsonObject temperature = doc.createNestedObject("temperature");
-  temperature["minSensorValue"] = temperatureMinSensorValue;
-  temperature["maxSensorValue"] = temperatureMaxSensorValue;
-  temperature["maxValue"] = convertSensorValueToTemperature(temperatureMinSensorValue);
-  temperature["minValue"] = convertSensorValueToTemperature(temperatureMaxSensorValue);
-
-  JsonObject light = doc.createNestedObject("light");
-  light["minSensorValue"] = lightMinSensorValue;
-  light["maxSensorValue"] = lightMaxSensorValue;
-
-  char output[256];
-  serializeJson(doc, output);
-  Serial.println(output);
-}
-
 void sendTelemetryMessage(int temperatureSensorValue, int lightSensorValue)
 {
-  const int capacity = JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(2);
+  const int capacity = JSON_OBJECT_SIZE(2);
+
   StaticJsonDocument<capacity> doc;
-  doc["messageType"] = "TELEMETRY";
-
-  JsonObject temperature = doc.createNestedObject("temperature");
-  temperature["sensorValue"] = temperatureSensorValue;
-  temperature["value"] = convertSensorValueToTemperature(temperatureSensorValue);
-  temperature["status"] = temperatureSensorValue < temperatureMinSensorValue ? "LOW" : (temperatureSensorValue > temperatureMaxSensorValue ? "HIGH" : "STABLE");
-
-  JsonObject light = doc.createNestedObject("light");
-  light["sensorValue"] = lightSensorValue;
-  light["status"] = lightSensorValue < lightMinSensorValue ? "LOW" : (lightSensorValue > lightMaxSensorValue ? "HIGH" : "STABLE");
+  doc["temperature"] = convertSensorValueToTemperature(temperatureSensorValue);
+  doc["lightLevel"] = lightSensorValue;
 
   char output[256];
   serializeJson(doc, output);
